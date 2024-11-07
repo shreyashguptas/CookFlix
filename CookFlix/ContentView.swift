65f1fcff3f71df42afbfd1ac2977c650f6299634
Shreyash Gupta <shreyash.gupta@shreyashg.com>
2024-11-07 14:04:37 -0600

    Initial Commit

---
+++ CookFlix/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CookFlix
+//
+//  Created by Shreyash Gupta on 11/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CookFlix/CookFlixApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookFlixApp.swift
+//  CookFlix
+//
+//  Created by Shreyash Gupta on 11/7/24.
+//
+
+import SwiftUI
+
+@main
+struct CookFlixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
