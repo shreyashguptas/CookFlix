09661b297eef584f18246e72e168b4416a936955
Shreyash Gupta <shreyash.gupta@shreyashg.com>
2024-11-07 14:58:48 -0600

    Fixing warnings
    1. In @Ingredient.swift I get 'Immutable property will not be decoded because it is declared with an initial value which cannot be overwritten'
    2. In @Recipe.swift I get 'Immutable property will not be decoded because it is declared with an initial value which cannot be overwritten'

--- CookFlix/Models/Ingredient.swift
+++ CookFlix/Models/Ingredient.swift
@@ -1,8 +1,15 @@
 import Foundation
 
 struct Ingredient: Identifiable, Codable {
-    let id = UUID()
+    var id: UUID
     let name: String
     let quantity: Double
     let unit: MeasurementUnit
+    
+    init(id: UUID = UUID(), name: String, quantity: Double, unit: MeasurementUnit) {
+        self.id = id
+        self.name = name
+        self.quantity = quantity
+        self.unit = unit
+    }
 } 
--- CookFlix/Models/Recipe.swift
+++ CookFlix/Models/Recipe.swift
@@ -1,10 +1,19 @@
 import Foundation
 
 struct Recipe: Identifiable, Codable {
-    let id = UUID()
+    var id: UUID
     let title: String
     let summary: String
     let imageName: String
     var ingredients: [Ingredient]?
     var instructions: [String]?
+    
+    init(id: UUID = UUID(), title: String, summary: String, imageName: String, ingredients: [Ingredient]? = nil, instructions: [String]? = nil) {
+        self.id = id
+        self.title = title
+        self.summary = summary
+        self.imageName = imageName
+        self.ingredients = ingredients
+        self.instructions = instructions
+    }
 } 
